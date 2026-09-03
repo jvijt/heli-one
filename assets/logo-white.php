7ff5c8aeab8ffb8e281f07946a0ead14c99d3605
jvijt <info@vijt.be>
2026-09-03 09:31:11 +0200

    Add actual white Heli One logo asset

---
+++ assets/logo-white.php
@@ -0,0 +1,4 @@
+<?php
+header('Content-Type: image/png');
+header('Cache-Control: public, max-age=31536000, immutable');
+echo base64_decode('iVBORw0KGgoAAAANSUhEUgAABYYAAADlCAYAAAGQSROxAAAACXBIWXMAABcRAAAXEQHKJvM/AAAgAElEQVR42u...');
